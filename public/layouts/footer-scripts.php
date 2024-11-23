4c492cbdf52fb3ce0aacc428faca5771e725cd67
Ernesto Serrano <erseco@gmail.com>
2024-11-23 07:54:50 +0000

    Fix bar

--- public/layouts/footer-scripts.php
+++ public/layouts/footer-scripts.php
@@ -9,6 +9,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 
+
+<script src="https://cdn.jsdelivr.net/npm/simplebar@6.2.7/dist/simplebar.min.js"></script>
+
+
 <?php if ($decker_page == 'tasks' ) { // Only load datatables.net on tasks page ?>
 
 
