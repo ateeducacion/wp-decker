21cf3bfc6a7c9f51ff01f94dd89fe1d3672ac8e7
Ernesto Serrano <erseco@gmail.com>
2024-11-26 22:15:23 +0000

    Adding tests

--- tests/Pest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function isUnitTest() {
-	return ! empty( $GLOBALS['argv'] ) && '--group=unit' === $GLOBALS['argv'][1];
-}
-
-use Yoast\WPTestUtils\WPIntegration\TestCase;
-
-uses( TestCase::class );
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,89 +5,66 @@
  * @package Decker
  */
 
-// First we need to load the composer autoloader, so we can use WP Mock
-require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+// // First we need to load the composer autoloader, so we can use WP Mock
+// require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
-use Yoast\WPTestUtils\WPIntegration;
+// $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-// Bootstrap WP_Mock to initialize built-in features
-WP_Mock::setUsePatchwork( true );
-WP_Mock::bootstrap();
-
-
-// Simular funciones de WordPress necesarias para el entorno de pruebas
-WP_Mock::userFunction(
-	'get_option',
-	array(
-		'return' => 'default_value',
-	)
-);
-
-WP_Mock::userFunction(
-	'update_option',
-	array(
-		'return' => true,
-	)
-);
-
-WP_Mock::userFunction(
-	'add_option',
-	array(
-		'return' => true,
-	)
-);
-
-WP_Mock::userFunction(
-	'plugin_dir_path',
-	array(
-		'return' => function ( $file ) {
-			return dirname( $file ) . '/';
-		},
-	)
-);
-
-WP_Mock::userFunction(
-	'register_activation_hook',
-	array(
-		'return' => true,
-	)
-);
-
-WP_Mock::userFunction(
-	'register_deactivation_hook',
-	array(
-		'return' => true,
-	)
-);
-
-WP_Mock::userFunction(
-	'has_action',
-	array(
-		'return' => true,
-	)
-);
-
-
-// If your project does not use autoloading via Composer, include your files now
-
-// require_once dirname( __DIR__ ) . '/decker.php';
-
-// require_once dirname(__DIR__) . '/decker.php';
-
-
-
-/**
- * Manualmente cargar el plugin.
- */
-// function _manually_load_plugin() {
-// require dirname( __DIR__ ) . '/decker.php';
+// if ( ! $_tests_dir ) {
+//     $_tests_dir = 'wordpress-develop/tests/phpunit';
 // }
 
+// require_once $_tests_dir . '/includes/functions.php';
 
-// Utilizar 'tests_add_filter' para cargar manualmente el plugin.
-// tests_add_filter('muplugins_loaded', '_manually_load_plugin');
+// // Carga tu plugin
+// tests_add_filter( 'muplugins_loaded', function() {
+//     require dirname( dirname( __FILE__ ) ) . '/decker.php';
+// } );
+
+// require $_tests_dir . '/includes/bootstrap.php';
 
 
-require_once dirname( __DIR__ ) . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
+use VCR\VCR;
+use function WP_CLI\Utils\load_dependencies;
 
-WPIntegration\bootstrap_it();
+ini_set('memory_limit', '-1');
+set_time_limit(0);
+
+if (!defined('PHPUNIT_COMPOSER_INSTALL')) {
+    define('PHPUNIT_COMPOSER_INSTALL', __DIR__ . '/../vendor/autoload.php');
+}
+
+if (!defined('$_SESSION')) {
+    global $_SESSION;
+    $_SESSION = [];
+}
+
+require_once __DIR__.'/defines.php';
+$vendorDir = __DIR__.'/../vendor';
+require_once $vendorDir . '/autoload.php';
+// require_once __DIR__.'/../includes/language.php';
+
+if (class_exists('\WP_Post_Type') === false) {
+    class WP_Post_Type {}
+}
+
+if (class_exists('\wpdb') === false) {
+    class wpdb {}
+}
+
+if (class_exists('\WP_Widget') === false) {
+    require_once __DIR__.'/../vendor/johnpbloch/wordpress-core/wp-includes/class-wp-widget.php';
+}
+
+if (!defined('WP_CLI_ROOT')) {
+    define('WP_CLI_ROOT', $vendorDir . '/wp-cli/wp-cli');
+}
+
+include WP_CLI_ROOT . '/php/utils.php';
+include WP_CLI_ROOT . '/php/dispatcher.php';
+include WP_CLI_ROOT . '/php/class-wp-cli.php';
+include WP_CLI_ROOT . '/php/class-wp-cli-command.php';
+
+load_dependencies();
+
+VCR::configure()->setCassettePath(__DIR__.'/fixtures/vcr');
