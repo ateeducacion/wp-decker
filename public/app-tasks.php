0743b6e79bfde4b83eb103955046fb81a1b04f66
Ernesto Serrano <info@ernesto.es>
2024-12-17 13:30:23 +0000

    Allow search for username

--- public/app-tasks.php
+++ public/app-tasks.php
@@ -253,7 +253,8 @@ table#tablaTareas td:nth-child(4) {
 													foreach ( $task->assigned_users as $user ) {
 														$today_class = $user->today ? ' today' : '';
 														echo '<a href="javascript: void(0);" class="avatar-group-item' . esc_attr( $today_class ) . '" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="' . esc_attr( $user->display_name ) . '" data-bs-original-title="' . esc_attr( $user->display_name ) . '">';
-														echo '<img src="' . esc_url( get_avatar_url( $user->ID ) ) . '" alt="" class="rounded-circle avatar-xs">';
+														echo '<span class="d-none">' . esc_attr( $user->display_name ) . '</span>';
+														echo '<img src="' . esc_url( get_avatar_url( $user->ID ) ) . '" alt="' . esc_attr( $user->display_name ) . '" class="rounded-circle avatar-xs">';
 														echo '</a>';
 													}
 													echo '</div></td>';
