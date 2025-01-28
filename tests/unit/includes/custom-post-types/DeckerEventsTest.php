fa8cce98aaa508c9b9f8d9c54b195ea6f8424d6b
Ernesto Serrano <erseco@gmail.com>
2025-01-28 17:32:47 +0000

    Some changes still noworking

--- tests/unit/includes/custom-post-types/DeckerEventsTest.php
+++ tests/unit/includes/custom-post-types/DeckerEventsTest.php
@@ -222,6 +222,8 @@ class DeckerEventsTest extends WP_Test_REST_TestCase {
         $this->assertEquals(200, $response->get_status());
         
         // Verify post is correctly trashed
+        $this->assertNull(get_post($this->event_id));
+
         $this->assertEquals('trashed', get_post_status($this->event_id));
 
     }
